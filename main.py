8e552b24db228618bd965323cca7273fc5f309e6
Misha Serbenyuk <outsourcing.you@gmail.com>
2022-09-09 18:43:28 +0300

    Enable arguments

--- main.py
+++ main.py
@@ -21,7 +21,6 @@ json_response = x.json()
 
 json_response[locize_version][language][locize_namespace].update(translated= json_response[locize_version][language][locize_namespace]['translated']*100, untranslated=json_response[locize_version][language][locize_namespace]['untranslated']*100)
 if (float(translation_percent) > json_response[locize_version][language][locize_namespace]['translated']):
-    print("Not enough translation")
     print(markdownTable([json_response[locize_version][language][locize_namespace]]).getMarkdown())
     sys.exit(1)
 elif (float(untranslated_percent) < json_response[locize_version][language][locize_namespace]['untranslated']  and float(untranslated_percent) !=0 ):
